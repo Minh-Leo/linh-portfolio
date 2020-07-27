16dcd9d1f485d29af1a40b00851a2d4af403995e
Minh Bui <tranduyminh.bui@gmail.com>
2020-07-27 21:29:43 +1000

    working on popup gallery

--- js/main.js
+++ js/main.js
@@ -16,3 +16,49 @@ const toggleSocialIcons = () => {
 
 btnTheme.addEventListener('click', toggleTheme);
 btnSocial.addEventListener('click', toggleSocialIcons);
+
+// // Modal Gallery
+// const showSlides = (n) => {
+//   let i;
+//   let slides = document.getElementsByClassName('mySlides');
+//   let dots = document.getElementsByClassName('demo');
+//   let captionText = document.getElementById('caption');
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].className += ' active';
+//   captionText.innerHTML = dots[slideIndex - 1].alt;
+// };
+
+// Open the Modal
+const openModal = () => {
+  document.getElementById('myModal').style.display = 'block';
+};
+
+// Close the Modal
+const closeModal = () => {
+  document.getElementById('myModal').style.display = 'none';
+};
+
+let slideIndex = 1;
+// showSlides(slideIndex);
+
+// Next/previous controls
+const plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+// Thumbnail image controls
+const currentSlide = (n) => {
+  showSlides((slideIndex = n));
+};
