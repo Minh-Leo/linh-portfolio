0344bece221ba6d8c0da02d5bafe876e3c59dc47
Minh Bui <tranduyminh.bui@gmail.com>
2020-08-02 23:07:34 +1000

    added gallery 1 to 4

--- js/gallery-content.js
+++ js/gallery-content.js
@@ -1,23 +1,165 @@
 const contents = {
   1: [
     [
-      './media/projects/intro1.jpg',
+      './media/projects/AdelaideNight/adel1.jpg',
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
     ],
     [
-      './media/projects/intro2.jpg',
+      './media/projects/AdelaideNight/adel2.jpg',
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
     ],
     [
-      './media/projects/intro3.jpg',
+      './media/projects/AdelaideNight/adel3.jpg',
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
     ],
     [
-      './media/projects/intro2.jpg',
+      './media/projects/AdelaideNight/adel4.jpg',
       ' Explicabo commodi ex quis, quam corrupti consequatur, facilis <br/>   doloribus ',
     ],
     [
-      './media/projects/intro1.jpg',
+      './media/projects/AdelaideNight/adel5.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/AdelaideNight/adel6.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+  ],
+  2: [
+    [
+      './media/projects/HaPortrait/ha1.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha2.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha3.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha4.jpg',
+      ' Explicabo commodi ex quis, quam corrupti consequatur, facilis <br/>   doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha9.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha10.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha11.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/HaPortrait/ha13.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+  ],
+  3: [
+    [
+      './media/projects/Cibi/cib1.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib2.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib3.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib4.jpg',
+      ' Explicabo commodi ex quis, quam corrupti consequatur, facilis <br/>   doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib5.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib6.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib7.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib8.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib9.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib10.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib11.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib12.jpg',
+      ' Explicabo commodi ex quis, quam corrupti consequatur, facilis <br/>   doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib13.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib14.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib15.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Cibi/cib16.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+  ],
+  4: [
+    [
+      './media/projects/Sea color/sea1.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea2.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea3.jpg',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea4.jpg',
+      ' Explicabo commodi ex quis, quam corrupti consequatur, facilis <br/>   doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea5.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea6.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea7.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea8.jpg',
+      'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
+    ],
+    [
+      './media/projects/Sea color/sea9.jpg',
       'licabo commodi ex quis, quam corrupti consequatur, facilis    doloribus ',
     ],
   ],
